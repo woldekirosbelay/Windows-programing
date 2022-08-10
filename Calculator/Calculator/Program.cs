bcb3db44f6eac763e527d064d0347faa2d53abf8
woldekirosbelay <110893986+woldekirosbelay@users.noreply.github.com>
2022-08-10 17:46:36 +0300

    The first assignments

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,61 @@
+﻿Console.WriteLine("WELOCOME TO THE CALCULATOR\nInput using these format\n\t\ta(+-*/)b");
+String equation;
+String left="", right="";
+bool operatorFound = false;
+int operatorIndex = 0;
+char operators='+';
+double result=0;
+
+while (true)
+{
+    Console.WriteLine("Input");
+    //getting input equation
+    equation = Console.ReadLine();
+    //getting left side
+    for (int i = 0; i < equation.Length; i++)
+    {
+        if (equation[i] == '+' || equation[i] == '-' || equation[i] == '*' || equation[i] == '/')
+        {
+            operatorFound = true;
+            operatorIndex = i;
+            operators = equation[i];
+        }
+
+        if (operatorFound == false)
+        {
+            left += equation[i];
+        }
+    }
+
+    //getting right side
+    for (int i = 0; i < equation.Length; i++)
+    {
+        if (i > operatorIndex)
+        {
+            right += equation[i];
+        }
+    }
+
+    //calculating result
+    if (operators == '+')
+    {
+        result = int.Parse(left) + int.Parse(right);
+    }
+
+    if (operators == '-')
+    {
+        result = int.Parse(left) - int.Parse(right);
+    }
+
+    if (operators == '*')
+    {
+        result = int.Parse(left) * int.Parse(right);
+    }
+
+    if (operators == '/')
+    {
+        result = int.Parse(left) / int.Parse(right);
+    }
+
+    Console.WriteLine("Result is " + result + "\n\n");
+}
